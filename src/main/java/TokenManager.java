34037fd597cebfe91f600548ff23599ae91f3dfe
Eric Hotaling <eric.hotaling@outlook.com>
2024-02-19 16:58:33 -0500

    Created TokenManager class, implemented Optional<Token> Peek(int j) method, boolean moreTokens() method, and constructor Began Optional<Token> matchAndRemove(Token.TokenType t) method

---
+++ src/main/java/TokenManager.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+import java.util.Optional;
+public class TokenManager {
+
+    private LinkedList<Token> tokens;
+    private int currentTokenIndex;
+
+    public TokenManager(LinkedList<Token> tokens) {
+        this.tokens = tokens;
+        this.currentTokenIndex = 0;
+    }
+
+    // peek “j” tokens ahead and return the token if we
+    // aren’t past the end of the token list.
+    Optional<Token> Peek(int j) {
+        int peekIndex = currentTokenIndex + j;
+        if (peekIndex < tokens.size()) {
+            return Optional.of(tokens.get(peekIndex));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    // returns true if the token list is not empty
+    boolean moreTokens() {
+        return currentTokenIndex < tokens.size();
+    }
+
+    // looks at the head of the list. If the token type of the head
+    // is the same as what was passed in, remove that token from the
+    // list and return it. In all other cases, returns Optional.Empty().
+    Optional<Token> matchAndRemove(Token.TokenType t) {
+
+    }
+}
